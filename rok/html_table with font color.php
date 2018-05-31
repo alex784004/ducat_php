a9af6108f361dcc7121c790a527ef0d32744221d
alexus <kumarsubham575@gmail.com>
2018-05-31 18:05:41 +0530

    Add files via upload

---
+++ rok/html_table with font color.php
@@ -0,0 +1,205 @@
+<html>
+<head>
+  <title>1st page</title>
+</head>
+<body>
+
+<table border="1" style="color: #00FFFF;" bgcolor="#000033">
+<tr bgcolor="#00FFFF" style="color:#000000">
+
+<th>S.NO.:</th>
+<th>First Name</th>
+<th>Last Name</th>
+<th>Password</th>
+<th>Email Address</th>
+<th>Mobile No</th>
+<th>Address: </th>
+<th>DOB:</th>
+<th>Gender:</th>
+<th>Country:</th>
+<th>Image:</th>
+<th>Action:</th>
+</tr>
+
+
+<tr>
+
+<td>1</td>
+<td>pretty</td>
+<td>singh</td>
+<td>asdf</td>
+<td>kumarsubham575@gmail.com</td>
+<td>9911205300</td>
+<td>delhi</td>
+<td>13/06/1998</td>
+<td>male</td>
+<td>india</td>
+<td><img src="118b6bd8-557d-4a9a-8323-227873e4601a.jpg" height="50" width="50"></td>
+<td><a href="#">edit</a></br>
+ <a href="#">delete</a></td>
+</tr>
+<tr>
+
+<td>1</td>
+<td>pretty</td>
+<td>singh</td>
+<td>asdf</td>
+<td>kumarsubham575@gmail.com</td>
+<td>9911205300</td>
+<td>delhi</td>
+<td>13/06/1998</td>
+<td>male</td>
+<td>india</td>
+<td><img src="118b6bd8-557d-4a9a-8323-227873e4601a.jpg" height="50" width="50"></td>
+<td><a href="#">edit</a></br>
+ <a href="#">delete</a></td>
+</tr>
+<tr>
+
+<td>1</td>
+<td>pretty</td>
+<td>singh</td>
+<td>asdf</td>
+<td>kumarsubham575@gmail.com</td>
+<td>9911205300</td>
+<td>delhi</td>
+<td>13/06/1998</td>
+<td>male</td>
+<td>india</td>
+<td><img src="118b6bd8-557d-4a9a-8323-227873e4601a.jpg" height="50" width="50"></td>
+<td><a href="#">edit</a></br>
+ <a href="#">delete</a></td>
+</tr>
+<tr>
+
+<td>1</td>
+<td>pretty</td>
+<td>singh</td>
+<td>asdf</td>
+<td>kumarsubham575@gmail.com</td>
+<td>9911205300</td>
+<td>delhi</td>
+<td>13/06/1998</td>
+<td>male</td>
+<td>india</td>
+<td><img src="118b6bd8-557d-4a9a-8323-227873e4601a.jpg" height="50" width="50"></td>
+<td><a href="#">edit</a></br>
+ <a href="#">delete</a></td>
+</tr>
+<tr>
+
+<td>1</td>
+<td>pretty</td>
+<td>singh</td>
+<td>asdf</td>
+<td>kumarsubham575@gmail.com</td>
+<td>9911205300</td>
+<td>delhi</td>
+<td>13/06/1998</td>
+<td>male</td>
+<td>india</td>
+<td><img src="118b6bd8-557d-4a9a-8323-227873e4601a.jpg" height="50" width="50"></td>
+<td><a href="#">edit</a></br>
+ <a href="#">delete</a></td>
+</tr>
+<tr>
+
+<td>1</td>
+<td>pretty</td>
+<td>singh</td>
+<td>asdf</td>
+<td>kumarsubham575@gmail.com</td>
+<td>9911205300</td>
+<td>delhi</td>
+<td>13/06/1998</td>
+<td>male</td>
+<td>india</td>
+<td><img src="118b6bd8-557d-4a9a-8323-227873e4601a.jpg" height="50" width="50"></td>
+<td><a href="#">edit</a></br>
+ <a href="#">delete</a></td>
+</tr>
+<tr>
+
+<td>1</td>
+<td>pretty</td>
+<td>singh</td>
+<td>asdf</td>
+<td>kumarsubham575@gmail.com</td>
+<td>9911205300</td>
+<td>delhi</td>
+<td>13/06/1998</td>
+<td>male</td>
+<td>india</td>
+<td><img src="118b6bd8-557d-4a9a-8323-227873e4601a.jpg" height="50" width="50"></td>
+<td><a href="#">edit</a></br>
+ <a href="#">delete</a></td>
+</tr>
+<tr>
+
+<td>1</td>
+<td>pretty</td>
+<td>singh</td>
+<td>asdf</td>
+<td>kumarsubham575@gmail.com</td>
+<td>9911205300</td>
+<td>delhi</td>
+<td>13/06/1998</td>
+<td>male</td>
+<td>india</td>
+<td><img src="118b6bd8-557d-4a9a-8323-227873e4601a.jpg" height="50" width="50"></td>
+<td><a href="#">edit</a></br>
+ <a href="#">delete</a></td>
+</tr>
+<tr>
+
+<td>1</td>
+<td>pretty</td>
+<td>singh</td>
+<td>asdf</td>
+<td>kumarsubham575@gmail.com</td>
+<td>9911205300</td>
+<td>delhi</td>
+<td>13/06/1998</td>
+<td>male</td>
+<td>india</td>
+<td><img src="118b6bd8-557d-4a9a-8323-227873e4601a.jpg" height="50" width="50"></td>
+<td><a href="#">edit</a></br>
+ <a href="#">delete</a></td>
+</tr>
+<tr>
+
+<td>1</td>
+<td>pretty</td>
+<td>singh</td>
+<td>asdf</td>
+<td>kumarsubham575@gmail.com</td>
+<td>9911205300</td>
+<td>delhi</td>
+<td>13/06/1998</td>
+<td>male</td>
+<td>india</td>
+<td><img src="118b6bd8-557d-4a9a-8323-227873e4601a.jpg" height="50" width="50"></td>
+<td><a href="#">edit</a></br>
+ <a href="#">delete</a></td>
+</tr>
+<tr>
+
+<td>1</td>
+<td>pretty</td>
+<td>singh</td>
+<td>asdf</td>
+<td>kumarsubham575@gmail.com</td>
+<td>9911205300</td>
+<td>delhi</td>
+<td>13/06/1998</td>
+<td>male</td>
+<td>india</td>
+<td><img src="118b6bd8-557d-4a9a-8323-227873e4601a.jpg" height="50" width="50"></td>
+<td><a href="#">edit</a></br>
+ <a href="#">delete</a></td>
+</tr>
+
+</table>
+
+</body>
+</html>
